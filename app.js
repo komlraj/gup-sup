01910516cec2e6edd3fa31a87c79e34c88bc953a
kml1326 <kmlraj131996@gmail.com>
2018-12-27 16:53:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cors = require('cors');
+const path = require('path');
+
+const port = 8000;
+
+mongoose.connect('mongodb://localhost/toDo-fullStack', { useNewUrlParser: true }, function(err, connection) {
+  if(err) throw err;
+  else console.log('connected to mongodb');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send("hello")
+})
+
+// app.set('views', path.join(__dirname, './server/views'));
+// app.set('view engine', 'ejs');
+
+// app.use(session({
+//   secret: 'toDo fullStack',
+//   resave: true,
+//   saveUninitialized: true,
+//   cookie: {
+//     maxAge: 3600000,
+//   },
+//   store: new MongoStore({ url: 'mongodb://localhost/toDo-fullStack-session'})
+// }));
+
+if(process.env.NODE_ENV === 'development') {
+  var webpack = require('webpack');
+  var webpackConfig = require('./webpack.config');
+  var compiler = webpack(webpackConfig);
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+
+  app.use(require('webpack-hot-middleware')(compiler));
+}
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// require('./server/modules/passport')(passport);
+// app.use(cors());
+
+
+// app.use('/api', require('./server/routes/api'));
+// app.use(require('./server/routes/index'));
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
