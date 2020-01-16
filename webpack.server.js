e72507b0f82a66c5ed1fec46ea8c88623872b458
kml1326 <kmlraj131996@gmail.com>
2020-01-16 08:53:12 +0530

    try to solve ejs-loader problem

--- webpack.server.js
+++ webpack.server.js
@@ -3,15 +3,25 @@ const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
 const webpackNodeExternals = require("webpack-node-externals");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   target: "node",
+  node: {
+    __dirname: true,
+    __filename: true
+  },
   entry: "./app.js",
   output: {
     filename: "server.bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  externals: [webpackNodeExternals()]
+  externals: [webpackNodeExternals()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'ejs-loader!./server/views/index.ejs'
+    })
+  ]
 }
 
 module.exports = merge(baseConfig, config);
